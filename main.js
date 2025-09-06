2c259f1b339bdd7e9b2620e452bbf066d7ac72be
Md Adnan Ahsani <140944890+adnanahsani@users.noreply.github.com>
2025-09-06 16:43:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function initMap() {
+    // The location of Uluru
+    var russia = { lat: 3.51602, lng: -2.1969 };
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 20, center: russia });
+    var marker = new google.maps.Marker({ position: russia, map: map });
+}
+
+var MenuItems = document.getElementById("menu-items");
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle(){
+    if(MenuItems.style.maxHeight == "0"){
+        MenuItems.style.maxHeight = "200px";
+    } else {
+        MenuItems.style.maxHeight = "0px";
+    }
+}
